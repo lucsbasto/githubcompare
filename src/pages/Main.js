5ff9e2ef52099213b5e4bd936e1e892e7439ae69
Lucas Bastos <lucasbastos@gmail.com>
2020-03-20 11:26:58 -0300

    estilizando a div da pagina deixando centralizada

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../assets/logo.png";
 
-const Main = () => <h1>Coé</h1>;
+const Div = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 60px;
+`;
+
+const Main = () => (
+    <Div>
+        <img src={logo} alt="GitHub Compare" />
+        <form action="">
+            <input type="text" placeholder="usuário/repositório" />
+            <button type="submit">OK</button>
+        </form>
+    </Div>
+);
 
 export default Main;
